f835ea79f10fd7c01b2255414d6a00d6ff61a98e
Jimmy Byrd <jimmybyrd@gmail.com>
2012-08-30 18:43:48 -0400

    Setting up MVVM

---
+++ Source/MetroPass.UI/ViewModels/DatabaseLoadViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+
+namespace MetroPass.UI.ViewModels
+{
+    public class DatabaseLoadViewModel
+    {
+        public IStorageFile Database { get; private set; }
+        public IStorageFile KeyFile { get; private set; }
+
+        public ICommand PickDatabase
+        {
+            get
+            {
+                return new DelegateCommand(ExecutePickDatabase);
+            }
+        }
+
+        public ICommand PickKeyFile { get; set; }
+        public ICommand LoadDatabase { get; set; }
+
+        public DatabaseLoadViewModel()
+        {
+            
+        }
+
+        public async void ExecutePickDatabase()
+        {
+            FileOpenPicker openPicker = new FileOpenPicker();
+            openPicker.ViewMode = PickerViewMode.List;
+            openPicker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
+            openPicker.FileTypeFilter.Add(".key");
+            var file = await openPicker.PickSingleFileAsync();
+        }
+    }
+}
---
+++ Source/MetroPass.UI/ViewModels/DelegateCommand.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Windows.Input;
+
+
+
+namespace MetroPass.UI.ViewModels
+{
+    public class DelegateCommand : ICommand
+    {
+        private Action _action;
+
+        public DelegateCommand(Action action)
+        {
+            _action = action;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            _action();
+        }
+    }
+}
