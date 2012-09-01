0f64f101e2619960ba8f661977454df3f42abdbd
Jimmy Byrd <jimmybyrd@gmail.com>
2012-09-01 05:22:01 -0400

    Missed a file on last checkin

---
+++ Source/MetroPass.UI/Helpers/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetroPass.UI.Helpers
+{
+    public static class Extend
+    {
+        // Extend ObservableCollection<T> Class
+        public static void AddRange<U>(this ObservableCollection<object> o, IEnumerable<U> items)
+        {
+            foreach (var item in items)
+            {
+                o.Add(item as object);
+            }
+        }
+    }
+}
