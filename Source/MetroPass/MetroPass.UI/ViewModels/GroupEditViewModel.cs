89d8884b6035b6e619c2a9c86ecf97cea2768d80
Jimmy Byrd <jimmybyrd@gmail.com>
2012-09-19 19:03:39 -0400

    Added Edit Group View Model

---
+++ Source/MetroPass/MetroPass.UI/ViewModels/GroupEditViewModel.cs
@@ -0,0 +1,48 @@
+﻿using Caliburn.Micro;
+using MetroPass.Core.Model;
+using MetroPass.UI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetroPass.UI.ViewModels
+{
+    public class GroupEditViewModel : BaseScreen
+    {
+        private readonly IPageServices pageServices;
+
+        public GroupEditViewModel(IPageServices pageServices, INavigationService navigationService) : base(navigationService)
+        {
+            this.pageServices = pageServices;
+        }
+        PwGroup pwGroup;
+
+        public PwGroup Group
+        {
+        	get
+        	{
+                return pwGroup;
+        	}
+            set
+            {
+                pwGroup = value;
+                NotifyOfPropertyChange(() => Group);
+            }
+        }
+
+        private string myVar;
+
+        public string GroupName
+        {
+            get { return Group.Name; }
+            set
+            {
+            	Group.Name = value;
+                NotifyOfPropertyChange(() => GroupName);
+            }
+        }
+
+    }
+}
