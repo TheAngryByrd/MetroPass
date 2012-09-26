d6a599cd82a7ec961fe6d8986a3b906ae7cfc4e5
kevinkuebler <kevinkuebler@gmail.com>
2012-09-25 22:31:15 -0400

    Basic search working with Title, Username & Notes.

---
+++ Source/MetroPass/MetroPass.UI/Common/EnumerableExtensions.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using MetroPass.Core.Model;
+
+namespace MetroPass.UI.ViewModels
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<PwEntry> AllEntries(this PwGroup node)
+        {
+            //yield return node;
+            foreach (var entry in node.Entries) {
+                yield return entry;
+            }
+
+            foreach (var childNode in node.SubGroups)
+                foreach (var allItem in AllEntries(childNode))
+                    yield return allItem;
+        }
+
+        public static void ForEach<T>(this IEnumerable<T> items, Action<T> action)
+        {
+            foreach (var item in items)
+            {
+                action(item);
+            }
+        }
+    }
+}
--- Source/MetroPass/MetroPass.UI/ViewModels/SearchResultsViewModel.cs
+++ Source/MetroPass/MetroPass.UI/ViewModels/SearchResultsViewModel.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.Linq;
 using Caliburn.Micro;
+using MetroPass.Core.Model;
+using MetroPass.UI.DataModel;
 
 namespace MetroPass.UI.ViewModels
 {
     public class SearchResultsViewModel : BaseScreen
     {
-        public SearchResultsViewModel(INavigationService navigationService) : base(navigationService) { }
+        public SearchResultsViewModel(INavigationService navigationService) : base(navigationService)
+        {
+            Results = new ObservableCollection<PwEntry>();
+        }
 
         private string _queryText;
         public string QueryText
@@ -16,7 +22,20 @@ namespace MetroPass.UI.ViewModels
             {
                 this._queryText = value;
                 NotifyOfPropertyChange(() => QueryText);
+                //SearchEntries();
             }
+        }
+
+        public ObservableCollection<PwEntry> Results { get; private set; }
+
+        private void SearchEntries()
+        {
+            var root = PWDatabaseDataSource.Instance.PwDatabase.Tree.Group;
+
+            var matchingEntries = root.AllEntries()
+                .Where(e => e.Title.Contains(QueryText) || e.Notes.Contains(QueryText) || e.Username.Contains(QueryText));
+
+            matchingEntries.ForEach(e => Results.Add(e));
         }
     }
 }
