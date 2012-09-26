8e10dc49e22e585ebccae15aea2ed44d28f26bfb
Jimmy Byrd <jimmybyrd@gmail.com>
2012-09-26 12:20:32 -0400

    Setup Initial EntryEditViewModel

---
+++ Source/MetroPass/MetroPass.UI/ViewModels/EntryEditViewModel.cs
@@ -0,0 +1,99 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MetroPass.Core.Model;
+
+namespace MetroPass.UI.ViewModels
+{
+    public class EntryEditViewModel : BaseScreen
+    {
+        public EntryEditViewModel(INavigationService navigationService) : base(navigationService)
+        {
+
+        }
+        PwEntry pwEntry;
+
+        public PwEntry Entry
+        {
+        	get
+        	{
+                return pwEntry;
+        	}
+            set
+            {
+                pwEntry = value;
+                NotifyOfPropertyChange(() => Entry);
+            }
+        }
+
+        public string Title
+        {
+        	get
+        	{
+                return Entry.Title;
+        	}
+
+            set
+            {
+                this.Entry.Title = value;
+                NotifyOfPropertyChange(() => Title);
+            }
+        }
+        public string Username
+        {
+            get
+            {
+                return Entry.Username;
+            }
+
+            set
+            {
+                this.Entry.Username = value;
+                NotifyOfPropertyChange(() => Username);
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return Entry.Password;
+            }
+
+            set
+            {
+                this.Entry.Password = value;
+                NotifyOfPropertyChange(() => Password);
+            }
+        }
+
+        public string Url
+        {
+        	get
+        	{
+                return Entry.Url;
+        	}
+            set
+            {
+                this.Entry.Url = value;
+                NotifyOfPropertyChange(() => Url);
+            }
+        }
+
+        public string Notes
+        {
+        	get
+        	{
+                return Entry.Notes;
+        	}
+            set
+            {
+                Entry.Notes = value;
+                NotifyOfPropertyChange(() => Notes);
+            }
+        }
+    }
+}
