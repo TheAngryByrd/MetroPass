08cc380309510aae014b5c74e487864d2f0eaeec
kevinkuebler <kevinkuebler@gmail.com>
2012-10-20 17:56:26 -0400

    Refactored Entry Edit screen.
    The Confirm field is now used to determine if the password actually
    matches and prevents the Save button from being enabled if it's not.
    Only problem is it only updates the CanSave status when tabbing out of
    the password/confirm fields. WinRT is missing UpdateSourceTrigger!
    
    Also, went ahead and made Save navigate back to the previous screen
    (probably what the user will usually want). And fixed the screen so that
    it doesn't update the underlying database unless the user actually taps
    the Save button.

--- Source/MetroPass/MetroPass.UI/ViewModels/EntryEditViewModel.cs
+++ Source/MetroPass/MetroPass.UI/ViewModels/EntryEditViewModel.cs
@@ -1,127 +1,131 @@
 ﻿using Caliburn.Micro;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using MetroPass.Core.Model;
 using MetroPass.UI.DataModel;
+using System.Diagnostics;
 
 namespace MetroPass.UI.ViewModels
 {
     public class EntryEditViewModel : BaseScreen
     {
+        private readonly INavigationService _navigationService;
+
         public EntryEditViewModel(INavigationService navigationService) : base(navigationService)
         {
-
+            this._navigationService=navigationService;
         }
-        PwEntry pwEntry;
 
+        private PwEntry _pwEntry;
         public PwEntry Entry
         {
-        	get
-        	{
-                return pwEntry;
-        	}
+        	get { return _pwEntry; }
             set
             {
-                pwEntry = value;
+                _pwEntry = value;
+                Title = _pwEntry.Title;
+                Username = _pwEntry.Username;
+                Password = _pwEntry.Password;
+                Confirm = _pwEntry.Password;
+                Url = _pwEntry.Url;
+                Notes = _pwEntry.Notes;
                 NotifyOfPropertyChange(() => Entry);
             }
         }
 
+        private string _title;
         public string Title
         {
-        	get
-        	{
-                return Entry.Title;
-        	}
+        	get	{ return _title; }
 
             set
             {
-                this.Entry.Title = value;
+                _title = value;
                 NotifyOfPropertyChange(() => Title);
             }
         }
+
+        private string _userName;
         public string Username
         {
-            get
-            {
-                return Entry.Username;
-            }
+            get { return _userName; }
 
             set
             {
-                this.Entry.Username = value;
+                _userName = value;
                 NotifyOfPropertyChange(() => Username);
             }
         }
 
+        private string _password;
         public string Password
         {
-            get
-            {
-                return Entry.Password;
-            }
+            get { return _password; }
 
             set
             {
-                this.Entry.Password = value;
+                _password = value;
+                CanSave = value == Confirm;
                 NotifyOfPropertyChange(() => Password);
             }
         }
 
-        public string Url
+        private string _confirm;
+        public string Confirm
         {
-        	get
-        	{
-                return Entry.Url;
-        	}
+            get { return _confirm; }
             set
             {
-                this.Entry.Url = value;
+                _confirm = value;
+                CanSave = value == Password;
+                NotifyOfPropertyChange(() => Password);
+            }
+        }
+
+        private string _url;
+        public string Url
+        {
+        	get { return _url; }
+            set
+            {
+                _url = value;
                 NotifyOfPropertyChange(() => Url);
             }
         }
 
+        private string _notes;
         public string Notes
         {
-        	get
-        	{
-                return Entry.Notes;
-        	}
+        	get { return _notes; }
             set
             {
-                Entry.Notes = value;
+                _notes = value;
                 NotifyOfPropertyChange(() => Notes);
             }
         
         }
 
-        private bool canSave = true;
+        private bool _canSave = true;
         public bool CanSave
         {
-            get
-            {
-                return canSave;
-            }
+            get { return _canSave; }
             set
             {
-                canSave = value;
+                _canSave = value;
                 NotifyOfPropertyChange(() => CanSave);
             }
         }
 
- 
-
         public async void Save()
         {
             CanSave = false;
-    
-                await PWDatabaseDataSource.Instance.SavePwDatabase();
-
-                CanSave = true;
-    
+            Entry.Title = Title;
+            Entry.Username = Username;
+            Entry.Password = Password;
+            Entry.Url = Url;
+            Entry.Notes = Notes;
+            await PWDatabaseDataSource.Instance.SavePwDatabase();
+            _navigationService.GoBack();
         }
     }
 }
