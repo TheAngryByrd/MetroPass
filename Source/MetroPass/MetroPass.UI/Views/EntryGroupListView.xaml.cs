9af07e1c830367bea1181c2797099906af3b935b
kevinkuebler <kevinkuebler@gmail.com>
2012-11-28 22:52:12 -0500

    Deselect item when closing app bar.
    Fixes #25

--- Source/MetroPass/MetroPass.UI/Views/EntryGroupListView.xaml.cs
+++ Source/MetroPass/MetroPass.UI/Views/EntryGroupListView.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Linq;
+﻿using MetroPass.UI.ViewModels;
 using Windows.UI.Xaml.Controls;
 
 namespace MetroPass.UI.Views
@@ -9,6 +8,16 @@ namespace MetroPass.UI.Views
         public EntryGroupListView()
         {
             this.InitializeComponent();
+        }
+
+        //HACK: This is wired up to a Caliburn Action, but it's not working due to an issue with the Windows.UI.Interactivity library
+        private void EntryAppBar_Closed(object sender, object e)
+        {
+            var vm = this.DataContext as EntryGroupListViewModel;
+            if (vm != null)
+            {
+                vm.DeselectItem();
+            }
         }
     }
 }
