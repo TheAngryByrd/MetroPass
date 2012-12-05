8f21348a24e407c43aecf47e23ad4cc52607d5ad
kevinkuebler <kevinkuebler@gmail.com>
2012-12-04 22:45:20 -0500

    Started adding settings for Recycle Bin.
    Added CaliburnMicroCallistoHelper nuget package which adds some helpers
    for using Callisto dialogs and settings flyouts with Caliburn. Started
    created an "Options" settings panel for MetroPass.

---
+++ Source/MetroPass/MetroPass.UI/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Caliburn.Micro;
+
+namespace MetroPass.UI.ViewModels
+{
+    public class SettingsViewModel : Screen
+    {
+        public SettingsViewModel()
+        {
+            this.DisplayName = "Options";
+
+            RecycleBinEnabled = true;
+        }
+
+        private bool _recycleBinEnabled;
+        public bool RecycleBinEnabled
+        {
+            get { return _recycleBinEnabled; }
+            set
+            {
+                _recycleBinEnabled = value;
+                NotifyOfPropertyChange(() => RecycleBinEnabled);
+            }
+        }
+    }
+}
