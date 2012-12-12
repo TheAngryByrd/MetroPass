e4cb887e1441546bb4a8c9650f4457ba23c3f1e3
Jimmy Byrd <jimmybyrd@gmail.com>
2012-12-11 22:36:53 -0500

    Added Locking Settings

---
+++ Source/MetroPass/MetroPass.UI/DataModel/SettingsModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace MetroPass.UI.DataModel
+{
+    public static class SettingsModel
+    {
+
+        private static ApplicationDataContainer settings = ApplicationData.Current.RoamingSettings;
+    
+        private static T Get<T>([CallerMemberName] String propertyName = null)
+        {
+            T retVal = default(T);
+            if( settings.Values.ContainsKey(propertyName))
+            {
+                object val = settings.Values[propertyName];
+                if (val is T)
+                {
+                    retVal = (T)val;
+                }
+            }
+            return retVal;
+        }
+        private static void Set<T>(T value, [CallerMemberName] String propertyName = null)
+        {
+            settings.Values[propertyName] = value;        
+        }
+
+        public static int MinutesToLockDatabase
+        {
+            get
+            {
+                return Get<int>();
+            }
+            set
+            {
+                Set<int>(value);
+            }
+        }
+
+        public static int SecondsToClearClipboard
+        {
+
+            get
+            {
+                return Get<int>();
+            }
+            set
+            {
+                Set<int>(value);
+            }
+        }
+    }
+}
---
+++ Source/MetroPass/MetroPass.UI/Services/LockingService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using MetroPass.UI.DataModel;
+using MetroPass.UI.ViewModels;
+
+namespace MetroPass.UI.Services
+{
+    public class LockingService : ILockingService
+    {
+        private readonly INavigationService navigationService;
+
+        public LockingService(INavigationService navigationService)
+        {
+            this.navigationService = navigationService;
+        }
+
+        public void Lock()
+        {
+            navigationService.UriFor<StartPageViewModel>().Navigate();
+        }
+
+        public void OnResumeLock(DateTime suspendedDate)
+        {
+            TimeSpan ts = DateTime.Now.Subtract(suspendedDate);
+
+            if (SettingsModel.MinutesToLockDatabase > 0)
+            {
+                if (ts.Seconds > SettingsModel.MinutesToLockDatabase)
+                {
+                    Lock();
+                }
+            }            
+        }
+    }
+}
