85507b42ffd427f3d02a3eacc62a4ab333edfb94
Jimmy Byrd <jimmybyrd@gmail.com>
2012-12-12 20:33:07 -0500

    Started clearning up UI

---
+++ Source/MetroPass/MetroPass.UI/Common/Coverters/HasValueToVisibilityCoverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace MetroPass.UI.Common.Coverters
+{
+    public class HasValueToVisibilityCoverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            // TODO: Implement this method
+            var defaultValue = GetDefaultValue(value.GetType());
+
+            return (value.Equals( defaultValue)) ? Visibility.Collapsed : Visibility.Visible;
+
+            
+        }
+
+        public object GetDefaultValue(Type type)
+        {
+            return type.GetTypeInfo().IsValueType ? Activator.CreateInstance(type) : null;
+        } 
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            // TODO: Implement this method
+            return value;
+        }
+    }
+}
