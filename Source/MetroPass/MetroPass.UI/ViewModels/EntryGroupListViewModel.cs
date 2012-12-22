01e858c2cc82a4a4a8ac876f0851fd7478e6d727
Jimmy Byrd <jimmybyrd@gmail.com>
2012-12-21 20:17:23 -0500

    Removed Ads

--- Source/MetroPass/MetroPass.UI/ViewModels/EntryGroupListViewModel.cs
+++ Source/MetroPass/MetroPass.UI/ViewModels/EntryGroupListViewModel.cs
@@ -48,7 +48,6 @@ namespace MetroPass.UI.ViewModels
                 _root = value;
                 _topLevelGroups.Add(new PwGroup(value.Element, false));
                 _topLevelGroups.AddRange(value.SubGroups);
-                _topLevelGroups.Add(new AdGroup());
                 NotifyOfPropertyChange(() => Root);
             }
         }
