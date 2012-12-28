619ae2f8f40e752bb6cc5de14912a851fc052ae2
Jimmy Byrd <jimmybyrd@gmail.com>
2012-12-28 18:14:12 -0500

    Added Generator View

---
+++ Source/MetroPass/MetroPass.UI/ViewModels/PasswordGeneratorViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using MetroPass.Core.Interfaces;
+
+namespace MetroPass.UI.ViewModels
+{
+    public class PasswordGeneratorViewModel : Screen
+    {
+        private readonly IPasswordGenerator passwordGenerator;
+
+        public PasswordGeneratorViewModel(IPasswordGenerator passwordGenerator)
+        {
+            this.passwordGenerator = passwordGenerator;
+        }
+
+
+    }
+}
