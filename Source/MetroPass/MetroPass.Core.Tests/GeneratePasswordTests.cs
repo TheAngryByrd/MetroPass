6e8f5a2920988290a89d6e996606ee86ffc812c6
Jimmy Byrd <jimmybyrd@gmail.com>
2012-12-28 17:16:00 -0500

    Moved generator password to core

--- Source/MetroPass/MetroPass.Core.Tests/GeneratePasswordTests.cs
+++ Source/MetroPass/MetroPass.Core.Tests/GeneratePasswordTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MetroPass.Core.Security;
 using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
 using Windows.Security.Cryptography;
 
@@ -86,57 +87,5 @@ namespace MetroPass.Core.Tests
 
     }
 
-    public class PasswordGenerator
-    {
-        public const string Uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        public const string Lowercase = "abcdefhijklmnopqrstuvwxyz";
-        public const string Digits = "0123456789";
-        public const string Minus = "-";
-        public const string Underscore = "_";
-        public const string Space = " ";
-        public const string Special = "!@#$%^&*+=?,.";
-        public const string Brackets = "(){}[]<>";
-
-        
-        public Task<string> GeneratePasswordAsync(int length, string[] characterSet, string charactersToExclude = null)
-        {
-            return Task.Run<string>(() =>
-            {
-                return GeneratePassword(length,  characterSet, charactersToExclude);
-            }); 
-        }
-
-        public string GeneratePassword(int length, string[] characterSet, string charactersToExclude = null)
-        {
-            var joinedCharacterSet = string.Join("", characterSet);
-
-            joinedCharacterSet = ExcludeCharactersFromString(charactersToExclude, joinedCharacterSet);
-
-            string retVal = string.Empty;
-            var random = new Random();
-            for (int i = 0; i < length; i++)
-            {
-                var nextIndex = GenerateRandomNumber(joinedCharacterSet.Length);
-                retVal += joinedCharacterSet[nextIndex];
-            }
-            return retVal;
-        }
-  
-        private string ExcludeCharactersFromString(string charactersToExclude, string joinedCharacterSet)
-        {
-            if (!string.IsNullOrEmpty(charactersToExclude))
-            {
-                foreach (var item in charactersToExclude)
-                {
-                    joinedCharacterSet = joinedCharacterSet.Replace(item.ToString(), string.Empty);
-                }
-            }
-            return joinedCharacterSet;
-        }
-  
-        private int GenerateRandomNumber(int max)
-        {
-            return Math.Abs((int)CryptographicBuffer.GenerateRandomNumber()) % (max);
-        }
-    }
+ 
 }
