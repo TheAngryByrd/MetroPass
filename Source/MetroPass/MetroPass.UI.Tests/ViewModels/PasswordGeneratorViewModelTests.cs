2a9253cb850bd9b2d9f0b8f1f6e0cd6a1cec863e
Jimmy Byrd <jimmybyrd@gmail.com>
2012-12-28 21:07:19 -0500

    Added UI test project

---
+++ Source/MetroPass/MetroPass.UI.Tests/ViewModels/PasswordGeneratorViewModelTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MetroPass.UI.ViewModels;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace MetroPass.UI.Tests.ViewModels
+{
+    [TestClass]
+    public class PasswordGeneratorViewModelTests
+    {
+        [TestMethod]
+        public void CanGenerateCharacterSetFromSwitches()
+        {
+            var vm = new PasswordGeneratorViewModel(null,null);
+        }
+    }
+}
