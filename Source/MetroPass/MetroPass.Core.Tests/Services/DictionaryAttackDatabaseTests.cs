bb7de12607175b00cf36f68207b7e10c1a2fbdf7
Jimmy Byrd <jimmybyrd@gmail.com>
2013-08-08 00:10:47 -0400

    Added can write keepass database tests

--- Source/MetroPass/MetroPass.Core.Tests/Services/DictionaryAttackDatabaseTests.cs
+++ Source/MetroPass/MetroPass.Core.Tests/Services/DictionaryAttackDatabaseTests.cs
@@ -1,9 +1,18 @@
-﻿using Metropass.Core.PCL.Model.Kdb4.Keys;
+﻿using System.Diagnostics;
+using MetroPass.Core.Services.Kdb4.Writer;
+using MetroPass.WinRT.Infrastructure.Compression;
+using MetroPass.WinRT.Infrastructure.Encryption;
+using MetroPass.WinRT.Infrastructure.Hashing;
+using Metropass.Core.PCL.Encryption;
+using Metropass.Core.PCL.Model;
+using Metropass.Core.PCL.Model.Kdb4.Keys;
+using Metropass.Core.PCL.Model.Kdb4.Writer;
 using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using PCLStorage;
 using Windows.Storage;
 using Windows.ApplicationModel;
 using Windows.Storage.Search;
@@ -21,7 +30,7 @@ namespace MetroPass.Core.Tests.Services
         public async Task Init()
         {
 
-    
+
 
         }
 
@@ -30,7 +39,7 @@ namespace MetroPass.Core.Tests.Services
         public async Task HighEncryptionRoundDatabase()
         {
             var tree = await Scenarios.LoadDatabase(PasswordDatabasePath, DatabasePassword, KeyFileath);
-   
+
         }
 
         [TestMethod]
@@ -62,6 +71,73 @@ namespace MetroPass.Core.Tests.Services
 
             Assert.AreEqual(listOfFails.Count(), 7);
         }
- 
+
+        [TestMethod]
+        public async Task CanWriteKeePassFiles()
+        {
+            var queryOptions = new QueryOptions(CommonFileQuery.OrderByName, new[] { ".kdbx" });
+            queryOptions.FolderDepth = FolderDepth.Shallow;
+
+            var samples = await Package.Current.InstalledLocation.GetFolderAsync("Samples2");
+            var query = samples.CreateFileQueryWithOptions(queryOptions);
+            IReadOnlyList<StorageFile> fileList = await query.GetFilesAsync();
+
+            var writer = new Kdb4Writer(new Kdb4HeaderWriter(),
+
+                      new WinRTCrypto(CryptoAlgoritmType.AES_CBC_PKCS7),
+                      new MultiThreadedBouncyCastleCrypto(CryptoAlgoritmType.AES_ECB),
+                      new SHA256HasherRT(),
+                      new GZipFactoryRT());
+
+            var listOfFails = new List<object>();
+
+            foreach (var file in fileList)
+            {
+                PwDatabase database = null;
+                string lastFileToTry = file.Name;
+
+                await TryCleanup(lastFileToTry);
+                try
+                {
+
+                    database = await Scenarios.LoadDatabase(file, "password", null);
+
+                    var newFile = await KnownFolders.DocumentsLibrary.CreateFileAsync(lastFileToTry);
+                    await writer.Write(database, new WinRTFile(newFile));
+                    await Scenarios.LoadDatabase(newFile, "password", null);
+                }
+                catch (FormatException e)
+                {
+                    listOfFails.Add(lastFileToTry);
+                }
+                catch (Exception e)
+                {
+                    if(System.Diagnostics.Debugger.IsAttached)
+                    {
+                        Debugger.Break();
+                    }
+                }
+                finally
+                {
+                    TryCleanup(lastFileToTry);
+
+                }
+            }
+
+            Assert.AreEqual(listOfFails.Count(), 7);
+        }
+
+        private async Task TryCleanup(string lastFileToTry)
+        {
+            try
+            {
+                var temp = await KnownFolders.DocumentsLibrary.GetFileAsync(lastFileToTry);
+                await temp.DeleteAsync();
+            }
+            catch (Exception)
+            {
+            }
+        }
+
     }
 }
