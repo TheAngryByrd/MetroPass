26cfc27893069a5c3a5f2ac4db972c1e082ae4c0
Jimmy Byrd <jimmybyrd@gmail.com>
2013-08-28 17:00:25 -0400

    Added OpenDatabaseView

---
+++ Source/MetroPass/MetroPass.WP8.UI/ViewModels/OpenDatabaseViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetroPass.WP8.UI.ViewModels
+{
+    public class OpenDatabaseViewModel
+    {
+        private readonly INavigationService _navigationService;
+
+        public OpenDatabaseViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+    }
+}
