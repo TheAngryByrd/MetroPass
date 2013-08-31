bddb1da2468de3774b2845fdc9aa6dbe927e209d
Jimmy Byrd <jimmybyrd@gmail.com>
2013-08-30 22:52:32 -0400

    namspace cleanup

--- Source/MetroPass/MetroPass.WP8.UI/Views/DropboxAccessView.xaml.cs
+++ Source/MetroPass/MetroPass.WP8.UI/Views/DropboxAccessView.xaml.cs
@@ -9,10 +9,5 @@ namespace MetroPass.WP8.UI.Views
         {           
             InitializeComponent();
         }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Clipboard.SetText("54a0ce26bb7646e582a52b4faabbf8289b0f2b36e09d9ee8e9db9c62eb3eb042");
-        }
     }
 }
